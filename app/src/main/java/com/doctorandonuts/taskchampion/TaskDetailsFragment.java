757e588c598ad4eda59a79ad1d1614397451ff2f
Doctor-Andonuts <doctor-andonuts@users.noreply.github.com>
2015-08-24 10:56:46 -0400

    Removed fragment listener for now from detailsfragment

--- app/src/main/java/com/doctorandonuts/taskchampion/TaskDetailsFragment.java
+++ app/src/main/java/com/doctorandonuts/taskchampion/TaskDetailsFragment.java
@@ -20,7 +20,7 @@ import org.w3c.dom.Text;
 public class TaskDetailsFragment extends Fragment {
     private Task task;
 
-    private OnFragmentInteractionListener mListener;
+//    private OnFragmentInteractionListener mListener;
 
     // TODO: Rename and change types and number of parameters
     public static TaskDetailsFragment newInstance(String param1, String param2) {
@@ -79,37 +79,37 @@ public class TaskDetailsFragment extends Fragment {
         getActivity().getMenuInflater().inflate(R.menu.menu_task_details, menu);
     }
 
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        try {
-            mListener = (OnFragmentInteractionListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
+//    @Override
+//    public void onAttach(Activity activity) {
+//        super.onAttach(activity);
+//        try {
+//            mListener = (OnFragmentInteractionListener) activity;
+//        } catch (ClassCastException e) {
+//            throw new ClassCastException(activity.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
+//    }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
+//    @Override
+//    public void onDetach() {
+//        super.onDetach();
+//        mListener = null;
+//    }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction();
-    }
+//    /**
+//     * This interface must be implemented by activities that contain this
+//     * fragment to allow an interaction in this fragment to be communicated
+//     * to the activity and potentially other fragments contained in that
+//     * activity.
+//     * <p/>
+//     * See the Android Training lesson <a href=
+//     * "http://developer.android.com/training/basics/fragments/communicating.html"
+//     * >Communicating with Other Fragments</a> for more information.
+//     */
+//    public interface OnFragmentInteractionListener {
+//        // TODO: Update argument type and name
+//        public void onFragmentInteraction();
+//    }
 
     public void setTask(Task task) {
         this.task = task;
