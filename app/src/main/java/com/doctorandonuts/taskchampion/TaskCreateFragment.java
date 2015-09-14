c354972df78240f0e833d6537587007014b6b488
Doctor-Andonuts <doctor-andonuts@users.noreply.github.com>
2015-09-14 09:27:25 -0400

    Fixed issue #16

--- app/src/main/java/com/doctorandonuts/taskchampion/TaskCreateFragment.java
+++ app/src/main/java/com/doctorandonuts/taskchampion/TaskCreateFragment.java
@@ -1,11 +1,17 @@
 package com.doctorandonuts.taskchampion;
 
 import android.app.ActionBar;
+import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class TaskCreateFragment extends Fragment {
@@ -30,6 +36,17 @@ public class TaskCreateFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_task_create, container, false);
+        View createFragment = inflater.inflate(R.layout.fragment_task_create, container, false);
+
+        EditText description = (EditText) createFragment.findViewById(R.id.editDescription);
+        description.requestFocus();
+        showSoftKeyboard();
+
+        return createFragment;
+    }
+
+    private void showSoftKeyboard() {
+        InputMethodManager inputMethodManager = (InputMethodManager)  getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
     }
 }
